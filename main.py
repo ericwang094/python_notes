303da7da1fc656c2e76e823fa7029bf415ae7c73
Eric Wang <eric.wang@rea-group.com>
2021-08-15 21:05:09 +1000

    init command

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+my_list = [3,2,5,4,1]
+print(sorted(my_list))
+print(my_list)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
